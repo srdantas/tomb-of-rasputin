bea2be5e29de0c49a3c62fda939a92b5bd6637b8
Renato Vieira Dantas <rvdantas@uolinc.com>
2019-05-27 12:16:25 -0300

    design a new level for passing it

--- adventures/tomb_of_rasputin/adventure.py
+++ adventures/tomb_of_rasputin/adventure.py
@@ -1,10 +1,9 @@
 from pygame.sprite import Group, spritecollide
 
-from game.label import Label
+from adventures.tomb_of_rasputin.level import Level1
 from game.sprite.obstacle import Obstacle
 from game.sprite.player import Player
 from game.sprite.zombie import Zombie
-from game.tilemap import TiledMap
 
 
 def info(game):
@@ -26,24 +25,26 @@ class TombOfRasputin:
         self.bullets = Group()
         self.walls = Group()
 
-        self.map = TiledMap('assets/maps/tomb_of_rasputin/level_1.tmx')
-        self.map_image = self.map.make_map()
+        self._actual = Level1(self)
+
+        self.map = self._actual.map
+        self.map_image = self._actual.map.make_map()
         self.map_rect = self.map_image.get_rect()
 
         self.has_menu = False
         self.updated = False
 
-        for tile_object in self.map.tiled_map.objects:
-            if tile_object.name == 'wall':
-                self._create_obstacle(tile_object, self.walls)
-            elif tile_object.name == 'zombie':
-                self._create_zombie(tile_object)
-            elif tile_object.name == 'player':
-                self._create_player(tile_object)
+        self.player = None
+
+        # before all assert, create a game objects
+        self._actual.load_objects()
 
     def update(self):
-        self._info()
+        self._actual.info()
         self.game_over()
+
+        if self._actual.next_level():
+            print('next level')
 
         self.updated = True
 
@@ -54,22 +55,17 @@ class TombOfRasputin:
         if spritecollide(self.player, self.zombies, False) and self.updated:
             self.game.game_over = True
 
-    def _create_player(self, tile_object):
+    def create_player(self, tile_object):
         self.player = Player(self.game, tile_object.x * self.map.scale, tile_object.y * self.map.scale)
 
-    def _create_zombie(self, tile_object):
+    def create_zombie(self, tile_object):
         Zombie(self.game, (self.game.all_sprites, self.zombies), tile_object.x * self.map.scale,
                tile_object.y * self.map.scale)
 
-    def _create_obstacle(self, tile_object, group):
+    def create_obstacle(self, tile_object):
         return Obstacle(self.game,
-                        group,
+                        self.walls,
                         tile_object.x * self.map.scale,
                         tile_object.y * self.map.scale,
                         tile_object.width * self.map.scale,
                         tile_object.height * self.map.scale)
-
-    def _info(self):
-        self.game.infos.append(Label('Level 1', "assets/fonts/blocks.ttf", 10, 10, font_size=42))
-        self.game.infos.append(
-            Label(f'Zombies: {len(self.zombies)}', "assets/fonts/pixel_square.ttf", 10, 80, font_size=24))
--- adventures/tomb_of_rasputin/level.py
+++ adventures/tomb_of_rasputin/level.py
@@ -1,3 +1,25 @@
+from game.label import Label
+from game.tilemap import TiledMap
+
+
 class Level1:
-    def __init__(self):
-        pass
+    def __init__(self, adventure):
+        self.adventure = adventure
+        self.map = TiledMap('assets/maps/tomb_of_rasputin/level_1.tmx')
+
+    def load_objects(self):
+        for tile_object in self.map.tiled_map.objects:
+            if tile_object.name == 'wall':
+                self.adventure.create_obstacle(tile_object)
+            elif tile_object.name == 'zombie':
+                self.adventure.create_zombie(tile_object)
+            elif tile_object.name == 'player':
+                self.adventure.create_player(tile_object)
+
+    def info(self):
+        self.adventure.game.infos.append(Label('Level 1', "assets/fonts/blocks.ttf", 10, 10, font_size=42))
+        self.adventure.game.infos.append(
+            Label(f'Zombies: {len(self.adventure.zombies)}', "assets/fonts/pixel_square.ttf", 10, 80, font_size=24))
+
+    def next_level(self):
+        return None
